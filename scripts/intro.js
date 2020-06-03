cac42fd900843232911bf7304a023e9332813899
LuisSempre <luis.silva@viannasempre.com.br>
2020-06-02 22:29:54 -0300

    Day 2

---
+++ scripts/intro.js
@@ -0,0 +1,74 @@
+//coments
+// document.write("Hello")
+
+// variaveis
+
+// let myvar = "He"
+// const myconst = 'He'
+
+// document.write(myconst + myvar)
+
+//string 
+
+// const s1="String" 
+// const s2='String' 
+// const s3=`String`
+
+// document.write(s1, s2, s3)
+
+// const n1=1
+// const n2=2 
+// const n3=3
+
+// document.write(n1 + n3)
+
+//boolean - true - false
+
+// const bTrue = true
+// const bFalse = false
+
+// document.write(bTrue)
+
+//objeto possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolas = ["b", "a", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolas[1].name)
+
+//Functions
+//registrar
+// function sayMyName (name){
+//   document.write(name)
+// }
+// //execute
+// sayMyName('Luis')
+// sayMyName('Andre')
+// sayMyName('K')
+
+// const notFim = 10
+
+// if(notFim < 5){
+//     document.write('Re')
+//     //caminho1
+//    }else{
+//      document.write('Le')
+//      //caminho2
+//    }
+
+//loop repeticoes
+
+// for(i=0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
